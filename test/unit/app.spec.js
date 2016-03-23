52cf83512e68301c21c0d715822dae02aed84b46
daniel <daniel@gg.gg>
2016-03-23 10:57:51 +0100

    update app.spec.js

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -4,6 +4,13 @@ describe('app', function () {
     var app = window.app;
 
     describe('generateMessage', function () {
+        it('should return true when bla bla ', function () {
+            expect(app.generateMessage("Aga")).toEqual({vowel: 2, palindrome: true});
+        });
+
+         it('should return false when bla bla ', function () {
+             expect(app.generateMessage("Agnieszka")).toEqual({vowel: 4, palindrome: false});
+    });
 
     });
 
